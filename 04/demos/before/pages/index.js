4ef47302a4072fd0ba53be85b76ac007ff02c7c2
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-07-30 15:34:27 -0600

    manual styles

--- 04/demos/before/pages/index.js
+++ 04/demos/before/pages/index.js
@@ -1,5 +1,35 @@
 function HomePage() {
-  return <div>Welcome to Jim's Robot Shop!</div>
+  
+  const ctsStyles = { 
+    display: 'flex',
+    justifyContent: 'space-around'
+   };
+
+  return (
+    <div style={ctsStyles}>
+      <span>
+        Image 1
+      </span>
+      <span>
+        Image 2
+      </span>
+      <div>
+          <div>
+            Your artis name
+          </div>
+          <div>
+            <div>Album desings</div>
+          </div>
+          <button>Shop now</button>
+      </div>
+      <span>
+        Image 3
+      </span>
+      <span>
+        Image 4
+      </span>
+    </div>
+  )
 }
 
 export default HomePage
